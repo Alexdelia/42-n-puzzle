00846b949f24f7644032def0de15e67ad721e6c4
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-11 20:30:24 +0200

    going forward

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod puz;
 use puz::Puz;
 
 fn main() {
-    println!("Hello, world!");
-
     let p = Puz::new(3);
 
     p.print();
--- src/puz/mod.rs
+++ src/puz/mod.rs
@@ -1,19 +1,21 @@
+/*use rand::seq::SliceRandom;
+use rand::thread_rng;*/
+
 pub struct Puz {
     _size: u8,
     _board: Vec<u8>,
 }
 
 impl Puz {
-    fn new(size: u8) -> Puz {
+    pub fn new(size: u8) -> Puz {
         Puz {
             _size: size,
             // will need to generate a random permutation of 1..size^2
-            _board: vec![0; size.pow(2) as usize],
+            _board: (0..size.pow(2)).collect::<Vec<u8>>(), //.shuffle(&mut thread_rng()),
         }
     }
 
     pub fn print(&self) {
-        println!("Puzzle:");
         for x in 0..self._size {
             for y in 0..self._size {
                 print!("{} ", self._board[(x * self._size + y) as usize]);

1952f92c43920182cb80a2bf957298d72f4dfd82
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-13 16:06:31 +0200

    add `color.rs`

---
+++ src/color.rs
@@ -0,0 +1,6 @@
+const BOLD: &'static str = "\x1b[1m";
+const CLEAR: &'static str = "\x1b[0m";
+
+const RED: &'static str = "\x1b[31m";
+const GRE: &'static str = "\x1b[32m";
+const MAG: &'static str = "\x1b[35m";

2cf5ded58e2a6d74591a054fc11e5264bccf7077
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-14 12:33:57 +0200

    move impl

--- src/puz/mod.rs
+++ src/puz/mod.rs
@@ -10,11 +10,17 @@ pub mod target_type;
 type Token = u16;
 type Size = u8;
 
+struct Coord {
+    x: Size,
+    y: Size,
+}
+
 pub struct Puz {
     _size: Size,
     _board: Vec<Token>,
     _target: Vec<Token>,
     _solution: Vec<r#move::Move>,
+    _blank: Coord,
 }
 
 impl Puz {
@@ -24,16 +30,31 @@ impl Puz {
             _board: Vec::new(),
             _target: Vec::new(),
             _solution: Vec::new(),
+            _blank: Coord { x: 0, y: 0 },
         }
     }
 
     pub fn from(size: Size) -> Puz {
-        Puz {
+        let mut p = Puz {
             _size: size,
             // will need to generate a random permutation of 1..size^2
             _board: (0..(size as Token).pow(2)).collect::<Vec<Token>>(), //.shuffle(&mut thread_rng()),
             _target: target_type::get_target_snake(size),
             _solution: Vec::new(),
+            _blank: Coord { x: 0, y: 0 },
+        };
+        p._update_blank();
+        return p;
+    }
+
+    fn _update_blank(&mut self) {
+        let size = self._size as Token;
+        for i in 0..size.pow(2) {
+            if self._board[i as usize] == 0 {
+                self._blank.x = (i % size) as Size;
+                self._blank.y = (i / size) as Size;
+                break;
+            }
         }
     }
 
--- src/puz/move.rs
+++ src/puz/move.rs
@@ -6,3 +6,51 @@ pub enum Move {
     Left,
     Right,
 }
+
+impl Move {
+    pub fn get_opposite(&self) -> Move {
+        match self {
+            Move::Up => Move::Down,
+            Move::Down => Move::Up,
+            Move::Left => Move::Right,
+            Move::Right => Move::Left,
+        }
+    }
+}
+
+impl Puz {
+    fn _move(&mut self, m: Move) {
+        let size = self._size as usize;
+        let blank_index = self._blank.y as usize * size + self._blank.x as usize;
+        match m {
+            Move::Up => {
+                if self._blank.y == 0 {
+                    return;
+                }
+                self._board.swap(blank_index, blank_index - size);
+                self._blank.y -= 1;
+            }
+            Move::Down => {
+                if self._blank.y == self._size - 1 {
+                    return;
+                }
+                self._board.swap(blank_index, blank_index + size);
+                self._blank.y += 1;
+            }
+            Move::Left => {
+                if self._blank.x == 0 {
+                    return;
+                }
+                self._board.swap(blank_index, blank_index - 1);
+                self._blank.x -= 1;
+            }
+            Move::Right => {
+                if self._blank.x == self._size - 1 {
+                    return;
+                }
+                self._board.swap(blank_index, blank_index + 1);
+                self._blank.x += 1;
+            }
+        }
+    }
+}

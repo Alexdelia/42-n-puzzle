a85a311897e319e9351d85281a80a360488e3de1
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-15 22:05:15 +0200

    finish to split parse

--- src/parse.rs
+++ src/parse.rs
@@ -13,7 +13,7 @@ pub fn parse() -> Result<Puz, bool> {
     let av = args().collect::<Vec<String>>();
 
     if av.len() > 3 {
-        p = match parse_av3(av[3].as_str()) {
+        p = match parse_size(av[3].as_str()) {
             Ok(p) => p,
             Err(_) => return Err(false),
         };
@@ -27,75 +27,31 @@ pub fn parse() -> Result<Puz, bool> {
     }
 
     if av.len() > 2 {
-        match parse_av2(av[2].as_str()) {
+        match parse_strategy(av[2].as_str()) {
             Ok(s) => p.set_strategy(s),
             Err(_) => return Err(false),
         }
     }
 
-    if av.len() > 2 {
-        match av[2].as_str() {
-            "a" | "astar" => p.set_strategy(Strategy::AStar),
-            "g" | "greedy" => p.set_strategy(Strategy::Greedy),
-            "u" | "uniform" => p.set_strategy(Strategy::Uniform),
-            _ => {
-                err_no!(
-                    "\"{M}{a}{C}{B}\" is not a valid strategy",
-                    a = av[2],
-                    C = color::CLEAR,
-                    B = color::BOLD,
-                    M = color::MAG
-                );
-                usage::usage_strategy();
-                return Err(false);
-            }
-        };
-    }
-
     if av.len() > 4 {
-        match av[4].as_str() {
-            "s" | "snail" => p.set_target(&target_type::get_target_snail(p.get_size())),
-            "c" | "classic" => p.set_target(&target_type::get_target_classic(p.get_size())),
-            _ => {
-                if !p.read(av[4].as_str(), true) {
-                    err_no!(
-                        "\"{M}{a}{C}{B}\" is not a valid goal_state",
-                        a = av[4],
-                        C = color::CLEAR,
-                        B = color::BOLD,
-                        M = color::MAG
-                    );
-                    usage::usage_goal_state();
-                    return Err(false);
-                }
-            }
-        };
+        if !parse_goal_state(av[4].as_str(), &mut p) {
+            return Err(false);
+        }
     } else {
         p.set_target(&target_type::get_target_snail(p.get_size()));
     }
 
     if av.len() > 5 {
-        match av[5].as_str() {
-            "t" | "true" => p.set_stop_at_first_solution(true),
-            "f" | "false" => p.set_stop_at_first_solution(false),
-            _ => {
-                err_no!(
-                    "\"{M}{a}{C}{B}\" is not a valid stop_at_first_solution",
-                    a = av[5],
-                    C = color::CLEAR,
-                    B = color::BOLD,
-                    M = color::MAG
-                );
-                usage::usage_stop_at_first_solution();
-                return Err(false);
-            }
-        }
+        match parse_stop_at_first_solution(av[5].as_str()) {
+            Ok(b) => p.set_stop_at_first_solution(b),
+            Err(_) => return Err(false),
+        };
     }
 
     return Ok(p);
 }
 
-fn parse_av3(a: &str) -> Result<Puz, bool> {
+fn parse_size(a: &str) -> Result<Puz, bool> {
     let mut p: Puz;
 
     if File::open(a).is_ok() {
@@ -147,6 +103,64 @@ fn parse_heuristic(a: &str) -> Result<fn(&[Token], Size, &[Token]) -> u32, bool>
                 M = color::MAG
             );
             usage::usage_heuristic();
+            return Err(false);
+        }
+    }
+}
+
+fn parse_strategy(a: &str) -> Result<Strategy, bool> {
+    match a {
+        "a" | "astar" => Ok(Strategy::AStar),
+        "g" | "greedy" => Ok(Strategy::Greedy),
+        "u" | "uniform" => Ok(Strategy::Uniform),
+        _ => {
+            err_no!(
+                "\"{M}{a}{C}{B}\" is not a valid strategy",
+                a = a,
+                C = color::CLEAR,
+                B = color::BOLD,
+                M = color::MAG
+            );
+            usage::usage_strategy();
+            return Err(false);
+        }
+    }
+}
+
+fn parse_goal_state(a: &str, p: &mut Puz) -> bool {
+    match a {
+        "s" | "snail" => p.set_target(&target_type::get_target_snail(p.get_size())),
+        "c" | "classic" => p.set_target(&target_type::get_target_classic(p.get_size())),
+        _ => {
+            if !p.read(a, true) {
+                err_no!(
+                    "\"{M}{a}{C}{B}\" is not a valid goal_state",
+                    a = a,
+                    C = color::CLEAR,
+                    B = color::BOLD,
+                    M = color::MAG
+                );
+                usage::usage_goal_state();
+                false;
+            }
+        }
+    }
+    return true;
+}
+
+fn parse_stop_at_first_solution(a: &str) -> Result<bool, bool> {
+    match a {
+        "t" | "true" => Ok(true),
+        "f" | "false" => Ok(false),
+        _ => {
+            err_no!(
+                "\"{M}{a}{C}{B}\" is not a valid stop_at_first_solution",
+                a = a,
+                C = color::CLEAR,
+                B = color::BOLD,
+                M = color::MAG
+            );
+            usage::usage_stop_at_first_solution();
             return Err(false);
         }
     }
